b5996bb071de55ddab2ea3b3ada402d9057cab7c
yoon520 <gmlwjddbs123@naver.com>
2023-10-12 09:33:24 +0900

    메인화면 글쓰기 버튼

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path="/:land" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
