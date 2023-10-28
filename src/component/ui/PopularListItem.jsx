e2981a4c5b3cd105405f75eec4aa4f4ca8ff6357
gachon sw academy <gachonswacademy03@gachonui-MacBookPro.local>
2023-10-28 21:55:10 +0900

    글 작성, 개인 홈 수정

--- src/component/ui/PopularListItem.jsx
+++ src/component/ui/PopularListItem.jsx
@@ -3,13 +3,24 @@ import "../../styles/PopularListItem.css";
 
 export default function PopularListItem(props) {
     const { post, onClick } = props;
+    
+    // 이미지 변환
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(post.contents, "text/html");
+    const imgElement = doc.querySelector("img");
+    const imgSrc = imgElement ? imgElement.getAttribute("src") : "default-image-url.jpg";
+
     return (
         <div className="popular-post" onClick={onClick}>
-            <img className="img" src={post.image} alt=" " />
+            {imgSrc !== "default-image-url.jpg" ? (
+                <img src={imgSrc} className="img" />
+            ) : (
+                <div className="noImg"></div>
+            )}
             <div className="post-contents">
                 <h2 className="title">{post.title}</h2>
                 <p>{post.summary}</p>
             </div>
         </div>
-    )
+    );
 }
