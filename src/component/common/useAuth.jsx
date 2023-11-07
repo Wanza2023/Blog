3457a07947e5870d05d89a044ee3c31b48210b21
gachon sw academy <gachonswacademy03@gachonui-MacBookPro.local>
2023-11-07 21:43:59 +0900

    로컬스토리지 토큰 구현 필요

---
+++ src/component/common/useAuth.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { isLoggedInState } from './AuthState';
+
+export const useAuth = () => {
+    const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInState);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, [setIsLoggedIn]);
+
+    return isLoggedIn;
+};
