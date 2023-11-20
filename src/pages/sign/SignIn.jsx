65cf928dd1b4bde5514ee385401406a6fbb5df1f
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-11-20 19:46:08 +0900

    비밀번호 변경 구현 및 css 수정

--- src/pages/sign/SignIn.jsx
+++ src/pages/sign/SignIn.jsx
@@ -40,8 +40,11 @@ function SignIn(props){
             .then(res =>{
                 setIsLoggedIn(true);    // 로그인 상태 전역관리 Login true
                 const token = res.data.body.token;
+                const email = res.data.body.email;
+
                 // setCookie('token',token,{path:'/'},{expires:1}); // 쿠키에 토큰 저장
                 sessionStorage.setItem('token', token); // 세션스토리지에 토큰 저장
+                sessionStorage.setItem('email', email);
                 axios
                     .get(`${process.env.REACT_APP_MEMBER_API_KEY}/authorize`, {
                         headers: {
@@ -49,9 +52,12 @@ function SignIn(props){
                         }
                     })
                     .then(response => {
+                        console.log(response.data.body);
                         setMemberId(response.data.body.id); // Token일치하는 사용자의 MemberId 전역변수로 저장
                         setNickName(response.data.body.nickName);   // Token일치하는 사용자의 nickname 전역변수로 저장
                         sessionStorage.setItem('nickName', response.data.body.nickName); // 세션스토리지에 닉네임 저장
+                        sessionStorage.setItem('birth', response.data.body.birth);
+                        sessionStorage.setItem('gender', response.data.body.gender);
                     })
                     .catch((err) => {
                         console.error("Error fetching data:", err);
