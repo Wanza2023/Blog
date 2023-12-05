a1b2d6999bf67f7d76d6d27d3861aff0e296f91f
naminhyeok <nmh9097@gachon.ac.kr>
2023-12-05 13:17:16 +0900

    hashtagList, PostList 인기글,최신순 갯수별조회 추가

--- src/pages/list/HashtagList.jsx
+++ src/pages/list/HashtagList.jsx
@@ -13,7 +13,7 @@ function HashtagList() {
   const [posts, setPosts] = useState([]); // 게시글 담을 배열 생성
   const [count, setCount] = useState(0); // 아이템 총 개수
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지. default 값으로 1
-  const [postPerPage] = useState(5); // 한 페이지에 보여질 아이템 수 
+  const [postPerPage, setPostPerPage] = useState(5); // 한 페이지에 보여질 아이템 수 
   const [currentPosts, setCurrentPosts] = useState(0); // 현재 페이지에서 보여지는 아이템들
 
   const navigate = useNavigate();
@@ -32,6 +32,10 @@ function HashtagList() {
     setCurrentPage(page);
     navigate(`?page=${page}`); // 해당 페이지로 이동
   };
+  const handlePostPerPageSelectChange = (e) => {
+    const selectedValue = parseInt(e.target.value);
+    setPostPerPage(selectedValue);
+  };
 
   useEffect(() => {
     if (hashtagLists !== undefined) {
@@ -47,7 +51,25 @@ function HashtagList() {
   return (
     <div className="wrapper">
       <div className='hashtag-title'>Hashtag</div>
-      {hashtag && <div className="hashtag-word"><HiOutlineHashtag size={15}/><div className="hashtag-word-term">{hashtag}&nbsp;에 대한 검색결과입니다.</div></div>}
+      {hashtag && 
+        <div className="postlist-topwrapper">
+          <div className="hashtag-word">
+            <HiOutlineHashtag size={15}/>
+            <div className="hashtag-word-term">{hashtag}&nbsp;에 대한 검색결과입니다.</div>
+          </div>
+          <div className="postlist-topwrapper">
+          <div>
+                <button className="postlist-popularbtn">인기순</button>
+                <button className="postlist-newestbtn">최신순</button>
+                <select className="postlist-postperpage-select" onChange={handlePostPerPageSelectChange}>
+                  <option value="5">5개</option>
+                  <option value="10">10개</option>
+                  <option value="20">20개</option>
+                </select>
+              </div>
+          </div>
+        </div>
+        }
       <div className='border-line' />
       {currentPosts && posts.length > 0 ? (currentPosts.map((item)=> // currentPosts가 있고, posts도 하나라도 있으면
         (<PostCard key={item.id} path={`/${item.nickname}/${item.boardId}`} {...item} />))):(<div className="resultNone">해시태그가 없습니다.</div>)}
