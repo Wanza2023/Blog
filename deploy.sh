377e7149e929a9f10380631b4532e92c4e2974e9
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-12-07 01:38:47 +0900

    스크립트 2차 수정

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# 배포된 파일들을 웹 루트 디렉토리로 이동
-mv /home/ubuntu/deploy/* /var/www/html/
-
 # 디렉토리로 이동
 cd /home/ubuntu/deploy
 
 # Yarn을 사용하여 종속성 설치
 yarn install
 
-# PM2를 사용하여 애플리케이션 재시작
-npx pm2 reload all
+# 리액트 애플리케이션 빌드
+yarn build
 
-# Nginx 재시작
+# 빌드된 파일들을 웹 루트 디렉토리로 이동
+mv build/* /var/www/html/
+
+# PM2와 Nginx 재시작
+npx pm2 reload all
 service nginx restart
