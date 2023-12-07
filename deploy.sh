8a636309aa8464487e6fd2183eb4cbf2284d9b31
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-12-07 12:22:14 +0900

    스크립트 9차 수정

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 cd /home/ubuntu/deploy
 
 # Yarn을 사용하여 종속성 설치
-npm install
+sudo npm install
 
 # 리액트 애플리케이션 빌드
-npm build
+sudo npm build
 
 # 빌드된 파일들을 웹 루트 디렉토리로 이동
 mv build/* /var/www/html/
 
 # PM2와 Nginx 재시작
-npx pm2 reload all
-service nginx restart
+sudo npx pm2 reload all
+sudo service nginx restart
