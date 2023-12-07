f41eb4899ee6323e476ca639bfae612c9305893c
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-12-07 14:50:35 +0900

    스크립트 12차 수정

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,8 @@ sudo npm install
 # 리액트 애플리케이션 빌드
 sudo npm run build
 
-# 빌드 결과 확인 및 이동
-if [ -d "build" ]; then
-    # 빌드된 파일들을 웹 루트 디렉토리로 이동
-    sudo mv build/* /var/www/html/
-else
-    echo "빌드 디렉토리가 존재하지 않습니다."
-fi
+# 빌드된 파일들을 웹 루트 디렉토리로 이동
+sudo mv /home/ubuntu/deploy/build/* /var/www/html/
 
 # PM2와 Nginx 재시작
 sudo npx pm2 reload all
