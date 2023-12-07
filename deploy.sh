06032bd0cccfc79d7dbb5b995425a5e85b0cdf60
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-12-07 12:32:35 +0900

    스크립트 10차 수정

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ cd /home/ubuntu/deploy
 sudo npm install
 
 # 리액트 애플리케이션 빌드
-sudo npm build
+sudo npm run build
 
 # 빌드된 파일들을 웹 루트 디렉토리로 이동
-mv build/* /var/www/html/
+sudo mv /home/ubuntu/deploy/build/* /var/www/html/
 
 # PM2와 Nginx 재시작
 sudo npx pm2 reload all
