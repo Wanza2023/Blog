8c46c8b66e8383073fd668bbb813fd2e128dd8a8
gachon sw academy <gachonswacademy03@gachonui-MacBookPro-4.local>
2023-12-10 22:58:24 +0900

    스크립트 재재재수정

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm install
 npm run build
 
 # 빌드된 파일들을 웹 루트 디렉토리로 이동
-sudo mv build/* /var/www/html/
+rsync -av --delete /home/ubuntu/deploy/build/ /var/www/html/
 
 # PM2와 Nginx 재시작
 sudo npx pm2 reload all
